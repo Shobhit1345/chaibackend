7b7a00f2df8df47e6fcdca90eebb419d0c7e2fd4
Shobhit Sharma <shobhitss172@gmail.com>
2025-04-13 11:50:04 +0530

    api

---
+++ src/utils/asuncHandler.js
@@ -0,0 +1,17 @@
+export {asyncHandler}
+
+
+
+
+
+const asyncHandler =(fun)=> async (req,res,next) =>{
+ try{
+  await fun(req,res,next)
+ }
+ catch(error){
+  res.sed(err.code || 500).json({
+    success: false,
+    message: err.message,
+  })
+ }
+}
